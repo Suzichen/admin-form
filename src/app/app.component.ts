704644057d6de22d3703946043a0ce9e03374c9d
suzichenapk@gmail.com <suzichenapk@gmail.com>
2017-10-26 12:04:32 +0800

    添加了ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,34 @@ import { DialogService } from "ngx-bootstrap-modal";
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    public username:string
+    public age:string
+    public nowIndex:number
+    public userData:any = []
     constructor(
         public dialogService:DialogService
     ) {}
 
+    add() {
+        this.userData.push({
+            username: this.username,
+            age: this.age
+        })
+        this.username = '';
+        this.age = '';
+        console.log(this.userData)
+    }
+    
     showConfirm() {
         this.dialogService.confirm('提醒', '确认要删除吗？', {
             // 可选项，可以对部分参数重写
+            confirmButtonClass: "btn-danger",
+            cancelButtonClass: "btn-primary"
         }).then((result: boolean) => {
             // result
+            if(result === false) return;
+            this.username = "haha"
+            this.age = "safsa"
         });
     }
 }
